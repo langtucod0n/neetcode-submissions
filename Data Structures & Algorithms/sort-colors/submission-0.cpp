9a7ce4eafc246837f2d9295957a01a6da61974c9
Võ Nhật Liệu <vnl2422006@gmail.com>
2026-07-13 16:39:53 +0700

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int one = 0;
+        int zero = 0;
+        for (int two = 0; two < nums.size(); two++) {
+            int temp = nums[two];
+            nums[two] = 2;
+            if (temp < 2) {
+                nums[one++] = 1;
+            }
+            if (temp < 1) {
+                nums[zero++] = 0;
+            }
+        }
+    }
+};
